6c0f194afd3cd5fc674ec2d945325524df0d3a3b
MonUkou <monukou666@gmail.com>
2026-02-24 09:18:59 +0700

    Update sp_thongKePhimTheoTheLoai.sql

--- docs/procedure/procedure_MonUkou/sp_thongKePhimTheoTheLoai.sql
+++ docs/procedure/procedure_MonUkou/sp_thongKePhimTheoTheLoai.sql
@@ -1,9 +1,8 @@
 BEGIN
     SELECT 
-        m.Movie_Tittle AS TenPhim,
-        m.Movie_ReleaseDate AS NgayPhatHanh,
-        g.Genre_Name AS TheLoai
-    FROM tbl_movie m
-    INNER JOIN tbl_genre g ON m.Genre_ID = g.Genre_ID
-    ORDER BY m.Movie_ReleaseDate DESC;
+        g.Genre_Name AS TheLoai,
+        COUNT(m.Movie_ID) AS SoLuongPhim
+    FROM tbl_genre g
+    LEFT JOIN tbl_movie m ON g.Genre_ID = m.Genre_ID
+    GROUP BY g.Genre_Name;
 END
