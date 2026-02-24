3cade49cf4e35a023b0af055a460aa9d950a4ad8
PNem06 <ptphuongnam06@gmail.com>
2026-02-24 20:54:39 +0700

    Update sp_TimPhimTheoTen.sql

--- docs/procedure/procedure_PNem06/sp_TimPhimTheoTen.sql
+++ docs/procedure/procedure_PNem06/sp_TimPhimTheoTen.sql
@@ -2,5 +2,5 @@ CREATE PROCEDURE SearchMovieByName(IN keyword VARCHAR(128))
 BEGIN
     SELECT * 
     FROM tbl_movie
-    WHERE Movie_Tittle LIKE CONCAT('%', keyword, '%');
+    WHERE Movie_Title LIKE CONCAT('%', keyword, '%');
 END
