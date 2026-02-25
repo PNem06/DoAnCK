6fa91a92acf39f69b7bfa0ee7946b6fb7898dd61
Tnhu:)) <nhutran.31241023442@st.ueh.edu.vn>
2026-02-25 10:55:13 +0700

    Create stored procedure for top movies by views

--- docs/procedure/procedure_tuyetnhu2006/sp_TopMoviesByViews.sql
+++ docs/procedure/procedure_tuyetnhu2006/sp_TopMoviesByViews.sql
@@ -1,1 +1,15 @@
-
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_TopMoviesByViews`(IN p_Limit INT)
+BEGIN
+    SELECT 
+        m.Movie_ID,
+        m.Movie_Title,
+        COUNT(w.Movie_ID) AS Total_Views
+    FROM tbl_movie m
+    LEFT JOIN tbl_watchlist w 
+        ON m.Movie_ID = w.Movie_ID
+    GROUP BY m.Movie_ID, m.Movie_Title
+    ORDER BY Total_Views DESC
+    LIMIT p_Limit;
+END$$
+DELIMITER ;
