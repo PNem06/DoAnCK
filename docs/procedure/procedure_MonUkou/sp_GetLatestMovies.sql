b11528e03af12b748eee0f5fe0c7bc4ac070a63b
MonUkou <monukou666@gmail.com>
2026-02-25 13:54:24 +0700

    Update and rename sp_phimMoiNhat.sql to sp_GetLatestMovies.sql

--- docs/procedure/procedure_MonUkou/sp_GetLatestMovies.sql
+++ docs/procedure/procedure_MonUkou/sp_GetLatestMovies.sql
@@ -1,8 +1,8 @@
 BEGIN
     SELECT 
-        m.Movie_Tittle AS TenPhim, 
-        m.Movie_ReleaseDate AS NgayPhatHanh,
-        g.Genre_Name AS TheLoai
+        m.Movie_Title AS MovieTitle, 
+        m.Movie_ReleaseDate AS ReleaseDate,
+        g.Genre_Name AS Genre
     FROM tbl_movie m
     INNER JOIN tbl_genre g ON m.Genre_ID = g.Genre_ID
     ORDER BY m.Movie_ReleaseDate DESC;
