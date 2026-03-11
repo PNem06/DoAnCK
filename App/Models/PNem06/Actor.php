71169de1b56a78ffcb7a4ad3865ac5563a1060ab
PNem06 <ptphuongnam06@gmail.com>
2026-03-11 07:58:33 +0700

    Add Actor class with methods for actor details

--- App/Models/PNem06/Actor.php
+++ App/Models/PNem06/Actor.php
@@ -1,1 +1,63 @@
+<?php
 
+class Actor {
+
+    private $conn;
+
+    private $id;
+    private $name;
+    private $info;
+
+    public function __construct($conn){
+        $this->conn = $conn;
+    }
+
+    public function setActor($id,$name,$info){
+        $this->id = $id;
+        $this->name = $name;
+        $this->info = $info;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getInfo(){
+        return $this->info;
+    }
+
+    // UML method
+    public function getCharacters($actor_id){
+
+        $sql = "
+        SELECT 
+            c.id,
+            c.name AS character_name,
+            m.title AS movie_title
+        FROM character c
+        JOIN movie m ON c.movie_id = m.id
+        WHERE c.actor_id = ?
+        ";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i",$actor_id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $characters = [];
+
+        while($row = $result->fetch_assoc()){
+            $characters[] = $row;
+        }
+
+        return $characters;
+    }
+
+}
+
+?>
